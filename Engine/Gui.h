d6e0f5c52a9fb406d51ba0aa5584e877a4d873cc
MurderBot1 <>
2025-03-29 22:11:07 -0700

    Sync mobile

--- Engine/Gui.h
+++ Engine/Gui.h
@@ -1,9 +1,43 @@
 // © 2025 Trent Cridland Murderbot1@outlook.com All Rights Reserved
 
 // C++ imported files
-
+#include <vector>
+#include <stdint>
 
 // Program imported files
 #include <wx/wx.h>
 
 // Function definitions
+class GuiLabel {
+    public:
+};
+
+class GuiButton {
+    public:
+        // Position and scale of button
+        float LocationRatioX;
+        float LocationRatioY;
+        float SizeRatioX;
+        float SizeRatioY;
+        
+        // Button style
+        char ButtonRColor, ButtonGColor, ButtonBColor;
+        char BoarderRColor, BoarderGColor, BoarderBColor;
+        char ClickedButtonRColor, ClickedButtonGColor, ClickedButtonBColor;
+        char ClickedBoarderRColor, ClickedBoarderGColor, ClickedBoarderBColor;
+        
+        // Label if one has been added
+        GuiLabel* Label;
+        GuiLabel* ClickedLabel;
+};
+
+class GuiFrame {
+    public:
+        // Store the other classes objects
+        std::vector<GuiButton*> Buttons;
+        std::vector<GuiLabel*> Labels;
+};
+
+class GuiCompile {
+    
+};
